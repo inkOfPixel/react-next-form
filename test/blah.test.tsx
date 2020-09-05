03b652a38273b89b80f654b239e5957cd15cabcd
macs <massimodemarchi91@gmail.com>
2020-09-05 12:10:47 +0200

    fake test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,21 +1,5 @@
-// import * as React from "react";
-// import * as ReactDOM from "react-dom";
-// import { useForm } from "../src";
-
-// function Test() {
-//   const form = useForm({
-//     initialValues: {},
-//   });
-//   React.useEffect(() => {
-//     console.log("LOG", form);
-//   }, [form]);
-//   return <div>Test</div>;
-// }
-
-// describe("it", () => {
-//   it("renders without crashing", () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<Test />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
-// });
+describe("it", () => {
+  it("Hello world", () => {
+    console.log("Hello world");
+  });
+});
