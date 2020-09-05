0f1fd7b716c354c9afba49ed5da70e1453306d35
macs <massimodemarchi91@gmail.com>
2020-09-05 11:30:58 +0200

    fix dummy test [skip ci]

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { useForm } from "../src";
 
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+function Test() {
+  const form = useForm({
+    initialValues: {},
+  });
+  React.useEffect(() => {
+    console.log("LOG", form);
+  }, [form]);
+  return <div>Test</div>;
+}
+
+describe("it", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Test />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
