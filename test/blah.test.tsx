30de8d5a1bd86e9505cfc6f089aa68c4375cdbda
macs <massimodemarchi91@gmail.com>
2020-09-05 12:07:30 +0200

    comment tests

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,21 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { useForm } from "../src";
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import { useForm } from "../src";
 
-function Test() {
-  const form = useForm({
-    initialValues: {},
-  });
-  React.useEffect(() => {
-    console.log("LOG", form);
-  }, [form]);
-  return <div>Test</div>;
-}
+// function Test() {
+//   const form = useForm({
+//     initialValues: {},
+//   });
+//   React.useEffect(() => {
+//     console.log("LOG", form);
+//   }, [form]);
+//   return <div>Test</div>;
+// }
 
-describe("it", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Test />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-});
+// describe("it", () => {
+//   it("renders without crashing", () => {
+//     const div = document.createElement("div");
+//     ReactDOM.render(<Test />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+//   });
+// });
